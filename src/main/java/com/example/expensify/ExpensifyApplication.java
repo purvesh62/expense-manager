fbcb876287178806c910216e19376da27d52ca59
Bansari Hitesh Shah <bn272375@dal.ca>
2022-06-24 16:14:07 -0300

    testing builds

--- src/main/java/com/example/expensify/ExpensifyApplication.java
+++ src/main/java/com/example/expensify/ExpensifyApplication.java
@@ -1,11 +1,9 @@
 package com.example.expensify;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ExpensifyApplication {
 
 	public static void main(String[] args) {
