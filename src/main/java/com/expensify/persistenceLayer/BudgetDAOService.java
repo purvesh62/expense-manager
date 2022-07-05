85f243e935ad38128d46abc059b767f1d0f7147b
Bansari Hitesh Shah <bn272375@dal.ca>
2022-07-05 12:11:33 -0300

    Budget APIS

--- src/main/java/com/expensify/persistenceLayer/BudgetDAOService.java
+++ src/main/java/com/expensify/persistenceLayer/BudgetDAOService.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 
 public class BudgetDAOService {
-    private final IDatabase mySqlDatabaseManager;
+    private final IDatabase database;
 
     public BudgetDAOService() {
-        this.mySqlDatabaseManager = Database.getInstance();
+        this.database = Database.getInstance();
     }
 
     public List<Budget> getAllBudgetDetails(int userId) throws SQLException {
@@ -22,7 +22,7 @@ public class BudgetDAOService {
         try {
             List<Object> parameterList = new ArrayList<>(10);
             parameterList.add(userId);
-            ResultSet resultSet = mySqlDatabaseManager.executeProcedure("CALL get_all_budget(?)", parameterList);
+            ResultSet resultSet = database.executeProcedure("CALL get_all_budget(?)", parameterList);
             if(resultSet!=null){
                 while (resultSet.next()) {
                     Budget budget = new Budget();
@@ -41,7 +41,7 @@ public class BudgetDAOService {
         catch (Exception e) {
             e.printStackTrace();
         } finally {
-            mySqlDatabaseManager.closeConnection();
+            database.closeConnection();
         }
         return budgetList;
     }
@@ -54,13 +54,13 @@ public class BudgetDAOService {
             parameterList.add(newBudget.getUserId());
             parameterList.add(newBudget.getBudgetLimit());
 
-           mySqlDatabaseManager.executeProcedure("CALL add_budget(?,?,?)", parameterList);
+            database.executeProcedure("CALL add_budget(?,?,?)", parameterList);
 
         }
         catch (Exception e) {
             e.printStackTrace();
         } finally {
-            mySqlDatabaseManager.closeConnection();
+            database.closeConnection();
         }
     }
 
@@ -71,13 +71,13 @@ public class BudgetDAOService {
             parameterList.add(budget.getWalletId());
             parameterList.add(budget.getBudgetLimit());
 
-            mySqlDatabaseManager.executeProcedure("CALL update_budget(?,?,?)", parameterList);
+            database.executeProcedure("CALL update_budget(?,?,?)", parameterList);
 
         }
         catch (Exception e) {
             e.printStackTrace();
         } finally {
-            mySqlDatabaseManager.closeConnection();
+            database.closeConnection();
         }
     }
 
@@ -86,12 +86,12 @@ public class BudgetDAOService {
             List<Object> parameterList = new ArrayList<>(10);
             parameterList.add(budgetId);
 
-            mySqlDatabaseManager.executeProcedure("CALL delete_budget(?)", parameterList);
+            database.executeProcedure("CALL delete_budget(?)", parameterList);
         }
         catch (Exception e) {
             e.printStackTrace();
         } finally {
-            mySqlDatabaseManager.closeConnection();
+            database.closeConnection();
         }
     }
 }
