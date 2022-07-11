9a0f7f190eddd3415a67dbb939d1b67dff304241
Bansari Hitesh Shah <bn272375@dal.ca>
2022-07-11 16:27:48 -0300

    Listing and edit update page done

--- src/main/java/com/expensify/controller/BudgetController.java
+++ src/main/java/com/expensify/controller/BudgetController.java
@@ -1,48 +1,56 @@
 package com.expensify.controller;
 
 import com.expensify.model.Budget;
+import com.expensify.model.BudgetFactory;
+import com.expensify.model.IBudgetFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class BudgetController {
 
-    private final Budget budget;
+//   To be asked :  private final IBudgetFactory budget  = new BudgetFactory();
+    private final Budget budget = new Budget();
 
-    public BudgetController() {
-        this.budget = new Budget();
-    }
 
 //    @RequestMapping(value="/api/v1/budget", method=RequestMethod.GET, produces="application/json")
 //    @ResponseBody
 //    private List<Budget> getAllBudgetDetails(@RequestParam("user_id") int userId) throws SQLException {
 //        return budget.getAllBudgetDetailsService(userId);
 //    }
-    @RequestMapping(value="/api/v1/budget", method=RequestMethod.POST, produces="application/json")
-    @ResponseBody
-    private Budget addBudget(@RequestBody Budget budgetObj) throws SQLException {
-        budget.saveBudget(budgetObj);
-        return budget;
+//    @RequestMapping(value="/api/v1/budget", method=RequestMethod.POST, produces="application/json")
+//    @ResponseBody
+//    private Budget addBudget(@RequestBody Budget budgetObj) throws SQLException {
+//        budget.createBudget().saveBudget(budgetObj);
+//        return budgetObj;
+//    }
+
+//    @RequestMapping(value="/api/v1/budget", method=RequestMethod.PUT, produces="application/json")
+//    @ResponseBody
+//    private Budget updateBudget(@RequestBody Budget budgetObj) throws SQLException {
+//        budget.updateBudget(budgetObj);
+//        return budget;
+//    }
+
+    @PostMapping(value="/api/v1/budget")
+    private String updateBudget(@ModelAttribute("budget") Budget budget) throws SQLException {
+        budget.updateBudget();
+        return "redirect:/api/v1/budget/1";
     }
 
-    @RequestMapping(value="/api/v1/budget", method=RequestMethod.PUT, produces="application/json")
-    @ResponseBody
-    private Budget updateBudget(@RequestBody Budget budgetObj) throws SQLException {
-        budget.updateBudget(budgetObj);
-        return budget;
-    }
-
-    @RequestMapping(value="/api/v1/budget", method=RequestMethod.DELETE, produces="application/json")
-    @ResponseBody
-    private void deleteBudget(@RequestParam("budget_id") int budgetId) throws SQLException {
-        budget.deleteBudget(budgetId);
-    }
-
+//    @RequestMapping(value="/api/v1/budget", method=RequestMethod.DELETE, produces="application/json")
+//    @ResponseBody
+//    private void deleteBudget(@RequestParam("budget_id") int budgetId) throws SQLException {
+//        budget.deleteBudget(budgetId);
+//    }
+//
     @GetMapping(value="/api/v1/budget/{user_id}", produces="text/html")
     public String getAllBudgetDetails(@PathVariable("user_id") int userId, Model model) throws SQLException {
         LocalDate currentdate = LocalDate.now();
@@ -51,6 +59,14 @@ public class BudgetController {
         System.out.println(startDate + endDate);
         List<Budget> budgetList = budget.getAllBudgetDetailsService(userId,startDate,endDate);
         model.addAttribute("budgetList" , budgetList);
-        return "Budget/budget";
+        return "budget";
+    }
+
+    @GetMapping(value="/api/v1/budget/budgetId/{budget_id}", produces="text/html")
+    private String getBudgetById(@PathVariable("budget_id") int budgetId, Model model) throws SQLException {
+       Budget b = budget.getBudgetById(budgetId);
+
+       model.addAttribute("budget",b);
+       return "updateBudget";
     }
 }
