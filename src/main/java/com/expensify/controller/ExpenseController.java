1a09fda8fc46182f2a05ada60b68ec4289583bc2
Purvesh Rathod <pr306467@dal.ca>
2022-07-13 17:49:54 -0300

    Added a condition in SessionManager to prevent null exception

--- src/main/java/com/expensify/controller/ExpenseController.java
+++ src/main/java/com/expensify/controller/ExpenseController.java
@@ -1,10 +1,13 @@
 package com.expensify.controller;
 
+import com.expensify.SessionManager;
 import com.expensify.model.Expense;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -18,35 +21,48 @@ public class ExpenseController {
         this.expense = new Expense();
     }
 
-    @RequestMapping(value = "/api/v1/expense", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "expense", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    List<Expense> get(@RequestParam(value = "user_id") int userID, @RequestParam(value = "start_date") String startDate, @RequestParam(value = "end_date") String endDate) {
+    List<Expense> get(@RequestParam(value = "start_date") String startDate, @RequestParam(value = "end_date") String endDate, HttpSession session) {
         try {
-            return this.expense.getAllUserExpenses(userID, startDate, endDate);
+            JSONObject userCache = SessionManager.getSession(session);
+            if (userCache.containsKey("userId")) {
+                int userId = (Integer) userCache.get("userId");
+                return this.expense.getAllUserExpenses(userId, startDate, endDate);
+            }
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
         return null;
     }
 
-    @RequestMapping(value = "/api/v1/expense", method = RequestMethod.POST, produces = "application/json")
+    @RequestMapping(value = "expense", method = RequestMethod.POST, produces = "application/json")
     @ResponseBody
-    Expense post(@RequestBody Expense expense) {
-        return expense.addUserExpense();
+    Expense post(@RequestBody Expense expense, HttpSession session) {
+        JSONObject userCache = SessionManager.getSession(session);
+        if (userCache.containsKey("userId")) {
+            int userId = (Integer) userCache.get("userId");
+            expense.setUserID(userId);
+            return expense.addUserExpense();
+        }
+        return null;
     }
 
 
-    @GetMapping(path = "/expenses/{userID}", produces = "text/html")
-    public String userExpenses(@PathVariable int userID, Model model) throws SQLException {
-        // Put data into model and return view name
-        LocalDate currentdate = LocalDate.now();
-        String startDate = currentdate.getYear() + "-01-" + currentdate.lengthOfMonth();
-        String endDate = currentdate.getYear() + "-" + currentdate.getMonth().ordinal() + "-" + currentdate.lengthOfMonth();
+    @GetMapping(path = "/expense", produces = "text/html")
+    public String userExpenses(@PathVariable int userID, Model model, HttpSession session) throws SQLException {
+        JSONObject userCache = SessionManager.getSession(session);
+        if (userCache.containsKey("userId")) {
+            LocalDate currentdate = LocalDate.now();
+            String startDate = currentdate.getYear() + "-01-" + currentdate.lengthOfMonth();
+            String endDate = currentdate.getYear() + "-" + currentdate.getMonth().ordinal() + "-" + currentdate.lengthOfMonth();
 
-        List<Expense> expenses = new ArrayList<>();
-        expenses = this.expense.getAllUserExpenses(userID, startDate, endDate);
-        model.addAttribute("expenses", expenses);
-        return "temp";
+            List<Expense> expenses = new ArrayList<>();
+            expenses = this.expense.getAllUserExpenses(userID, startDate, endDate);
+            model.addAttribute("expenses", expenses);
+            return "temp";
+        } else {
+            return "redirect:/login";
+        }
     }
-
 }
