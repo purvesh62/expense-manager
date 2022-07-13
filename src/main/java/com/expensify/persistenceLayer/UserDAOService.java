288cfd8759b61b3259a5fea04c2871a6fcc66b24
Purvesh Rathod <pr306467@dal.ca>
2022-07-13 17:56:26 -0300

    Renamed authentication file to User

--- src/main/java/com/expensify/persistenceLayer/UserDAOService.java
+++ src/main/java/com/expensify/persistenceLayer/UserDAOService.java
@@ -2,7 +2,7 @@ package com.expensify.persistenceLayer;
 
 import com.expensify.database.Database;
 import com.expensify.database.IDatabase;
-import com.expensify.model.Authentication;
+import com.expensify.model.User;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
@@ -12,22 +12,22 @@ import java.util.List;
 
 
 @Component
-public class AuthenticationDAO {
+public class UserDAOService {
     private final IDatabase database;
 
-    public AuthenticationDAO() {
+    public UserDAOService() {
         this.database = Database.getInstance();
     }
 
-    public int saveUser(Authentication authentication) throws SQLException {
+    public int saveUser(User user) throws SQLException {
         List<Object> parameterList = new ArrayList<>();
         int userId = 0;
         try {
-            parameterList.add(authentication.getFirstName());
-            parameterList.add(authentication.getLastName());
-            parameterList.add(authentication.getEmail());
-            parameterList.add(authentication.getPassword());
-            parameterList.add(authentication.getContact());
+            parameterList.add(user.getFirstName());
+            parameterList.add(user.getLastName());
+            parameterList.add(user.getEmail());
+            parameterList.add(user.getPassword());
+            parameterList.add(user.getContact());
 
             ResultSet resultSet = database.executeProcedure("CALL register_user(?, ?, ?, ?, ?)", parameterList);
             if (resultSet != null) {
@@ -46,16 +46,16 @@ public class AuthenticationDAO {
         return userId;
     }
 
-    public int verifyUser(Authentication authentication) throws SQLException {
+    public int verifyUser(User user) throws SQLException {
         List<Object> parameterList = new ArrayList<>();
         int userId = 0;
         try {
-            parameterList.add(authentication.getEmail());
+            parameterList.add(user.getEmail());
 
             ResultSet resultSet = database.executeProcedure("CALL get_user_credential(?)", parameterList);
             if (resultSet != null) {
                 while (resultSet.next()) {
-                    if (resultSet.getString("password").equals(authentication.getPassword())) {
+                    if (resultSet.getString("password").equals(user.getPassword())) {
                         userId = resultSet.getInt("user_id");
                     }
                 }
