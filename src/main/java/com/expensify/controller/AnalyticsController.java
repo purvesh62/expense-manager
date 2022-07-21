721acb452231662bcbcccf006078e477e3048ba4
Purvesh Rathod <pr306467@dal.ca>
2022-07-21 01:23:29 -0300

    Added abstract design pattern for expense class and added side bar in the home page

--- src/main/java/com/expensify/controller/AnalyticsController.java
+++ src/main/java/com/expensify/controller/AnalyticsController.java
@@ -1,6 +1,9 @@
 package com.expensify.controller;
 
+import com.expensify.database.Database;
 import com.expensify.model.Expense;
+import com.expensify.model.IExpense;
+import com.expensify.model.factories.ExpenseFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +21,17 @@ public class AnalyticsController {
         LocalDate currentdate = LocalDate.now();
         String startDate = currentdate.getYear() + "-" + (currentdate.getMonth().ordinal() + 1) + "-01";
         String endDate = currentdate.getYear() + "-" + (currentdate.getMonth().ordinal() + 1) + "-" + currentdate.lengthOfMonth();
-        Expense expenseObj = new Expense();
-        List<Expense> expenses = expenseObj.getAllUserExpenses(1, startDate, endDate);
+
+        List<IExpense> expenses = new ExpenseFactory().createExpense(Database.instance()).getAllUserExpenses(1, startDate, endDate);
         HashMap<String, Float> expenseCategoryMap = new HashMap<>();
 
-        for (Expense expense : expenses) {
-            if (expenseCategoryMap.containsKey(expense.getExpenseTitle())) {
-                expenseCategoryMap.put(expense.getExpenseTitle(), expenseCategoryMap.get(expense.getExpenseTitle()) + expense.getAmount());
-            } else {
-                expenseCategoryMap.put(expense.getExpenseTitle(), expense.getAmount());
-            }
-        }
+//        for ((IExpense expense :expenses){
+//            if (expenseCategoryMap.containsKey(expense.getExpenseTitle())) {
+//                expenseCategoryMap.put(expense.getExpenseTitle(), expenseCategoryMap.get(expense.getExpenseTitle()) + expense.getAmount());
+//            } else {
+//                expenseCategoryMap.put(expense.getExpenseTitle(), expense.getAmount());
+//            }
+//        }
         model.addAttribute("expenseData", expenses);
         model.addAttribute("expenseMap", expenseCategoryMap);
 //        model.addAttribute("chartData", getChartData());
