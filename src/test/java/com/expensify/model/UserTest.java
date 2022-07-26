97b8f76eea00efe46c8b0e3403408e383e8f2ca2
aishwaryan <as419275@dal.ca>
2022-07-26 17:59:27 -0300

    Updated Test cases

--- src/test/java/com/expensify/model/UserTest.java
+++ src/test/java/com/expensify/model/UserTest.java
@@ -14,20 +14,6 @@ import java.util.List;
 public class UserTest {
 
     @Test
-    public void resetPasswordSuccessTest() throws SQLException {
-        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
-        boolean result = userDAOServiceMock.resetPassword("project@dal.ca");
-        Assertions.assertEquals(true, result);
-    }
-
-    @Test
-    public void resetPasswordFailureTest() throws SQLException {
-        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
-        boolean result = userDAOServiceMock.resetPassword("");
-        Assertions.assertEquals(false, result);
-    }
-
-    @Test
     public void saveUserSuccessTest()throws SQLException {
         UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
         int result = userDAOServiceMock.saveUser("groupproject","asdc","group7asdc@dal.ca","group7asdc","9789098078");
@@ -39,11 +25,18 @@ public class UserTest {
         int result = userDAOServiceMock.saveUser("","asdc","group7asdcdal.ca","group7asdc","9789098078");
         Assertions.assertEquals(0, result);
     }
+
     @Test
-    public void verifyUserSuccessTest() throws SQLException{
+    public void getUserPasswordSuccessTest() throws SQLException{
         UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
-        int result = userDAOServiceMock.verifyUser("project@dal.ca");
-        Assertions.assertEquals(1, result);
+        boolean result = Boolean.parseBoolean(userDAOServiceMock.getUserPassword("project@dal.ca"));
+        Assertions.assertEquals(true, result);
+    }
+    @Test
+    public void getUserPasswordFailureTest() throws SQLException{
+        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
+        boolean result = Boolean.parseBoolean(userDAOServiceMock.getUserPassword(""));
+        Assertions.assertEquals(false, result);
     }
     @Test
     public void verifyUserFailureTest() throws SQLException{
@@ -58,22 +51,32 @@ public class UserTest {
         Assertions.assertEquals(true, result);
     }
     @Test
+    public void resetPasswordSuccessTest() throws SQLException {
+        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
+        boolean result = userDAOServiceMock.resetPassword("project@dal.ca");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void resetPasswordFailureTest() throws SQLException {
+        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
+        boolean result = userDAOServiceMock.resetPassword("");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void verifyUserSuccessTest() throws SQLException{
+        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
+        int result = userDAOServiceMock.verifyUser("project@dal.ca");
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
     public void updatePasswordFailureTest() throws SQLException{
         UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
         boolean result = userDAOServiceMock.updatePassword("project@dal.ca","");
         Assertions.assertEquals(false, result);
     }
-    @Test
-    public void getUserPasswordSuccessTest() throws SQLException{
-        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
-        boolean result = Boolean.parseBoolean(userDAOServiceMock.getUserPassword("project@dal.ca"));
-        Assertions.assertEquals(true, result);
-    }
-    @Test
-    public void getUserPasswordFailureTest() throws SQLException{
-        UserDAOServiceMock userDAOServiceMock = new UserDAOServiceMock();
-        boolean result = Boolean.parseBoolean(userDAOServiceMock.getUserPassword(""));
-        Assertions.assertEquals(false, result);
-    }
+
 
 }
