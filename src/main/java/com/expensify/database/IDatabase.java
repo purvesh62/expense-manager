ece062091b313a1881f5a7957b9c8aca78e2ce49
aishwaryan <as419275@dal.ca>
2022-07-26 21:36:59 -0300

    CSCI5308 Project Demo-prod branch

--- src/main/java/com/expensify/database/IDatabase.java
+++ src/main/java/com/expensify/database/IDatabase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IDatabase {
-    Connection connectDB();
+    Connection   connectDB();
 
     ResultSet executeProcedure(String procQuery, List<Object> parameterList) throws SQLException;
 
